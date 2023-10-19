05d8fc054d90e8145d079d19eb72024f7d2a1bf4
-- <---->
2023-10-19 13:43:57 +0300

    initial xcode project creation

---
+++ youtubeBild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtubeBild
+//
+//  Created by Татьяна Магнушевская on 19.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
